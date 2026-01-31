fec53086518cacfff3f0f25ac60606d4b90f3307
amiraldinA <amiraldin7@gmail.com>
2026-01-30 21:20:06 -0600

    Configuration for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // IMPORTANT: The value below must match your GitHub repository name exactly.
+  // If your repo is https://github.com/user/my-app, this should be '/my-app/'
+  base: '/lumina-app/', 
 })
